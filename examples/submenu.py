10c86c101db0a874df61b5fafd1ed1ce072ff19c
Robert Knight <bobby@rdknight.net>
2017-08-07 19:19:31 -0500

    Updated for GNOME Nautilus 3.22.3
    Tested with Fedora 25

--- examples/submenu.py
+++ examples/submenu.py
@@ -1,29 +1,38 @@
-import nautilus
+import os
+from gi.repository import Nautilus, GObject
+# https://github.com/GNOME/nautilus-python/blob/master/examples/submenu.py
 
-class ExampleMenuProvider(nautilus.MenuProvider):
-    
+class ExampleMenuProvider(GObject.GObject, Nautilus.MenuProvider):
     # Nautilus crashes if a plugin doesn't implement the __init__ method.
     # See Bug #374958
+
     def __init__(self):
         pass
-        
-    def get_file_items(self, window, files):
-        top_menuitem = nautilus.MenuItem('ExampleMenuProvider::Foo', 'Foo', '')
 
-        submenu = nautilus.Menu()
+    def get_file_items(self, window, file):
+
+        top_menuitem = Nautilus.MenuItem(name='ExampleMenuProvider::Foo',
+                                         label='Foo',
+                                         tip='',
+                                         icon='')
+
+        submenu = Nautilus.Menu()
         top_menuitem.set_submenu(submenu)
 
-        sub_menuitem = nautilus.MenuItem('ExampleMenuProvider::Bar', 'Bar', '')
+        sub_menuitem = Nautilus.MenuItem(name='ExampleMenuProvider::Bar',
+                                         label='Bar',
+                                         tip='',
+                                         icon='')
         submenu.append_item(sub_menuitem)
 
         return top_menuitem,
 
     def get_background_items(self, window, file):
-        submenu = nautilus.Menu()
-        submenu.append_item(nautilus.MenuItem('ExampleMenuProvider::Bar', 'Bar', ''))
+        submenu = Nautilus.Menu()
+        submenu.append_item(
+        Nautilus.MenuItem('ExampleMenuProvider::Bar', 'Bar', '', ''))
 
-        menuitem = nautilus.MenuItem('ExampleMenuProvider::Foo', 'Foo', '')
+        menuitem = Nautilus.MenuItem('ExampleMenuProvider::Foo', 'Foo', '', '')
         menuitem.set_submenu(submenu)
 
         return menuitem,
-

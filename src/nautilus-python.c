2775cf30cddcf569e1931366266364aa0c91b885
Jan Tojnar <jtojnar@gmail.com>
2022-08-10 20:05:27 +0200

    Remove autotools

--- src/nautilus-python.c
+++ src/nautilus-python.c
@@ -17,9 +17,7 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-#ifdef HAVE_CONFIG_H
-# include <config.h>
-#endif
+#include <config.h>
 
 #include <Python.h>
 #define NO_IMPORT_PYGOBJECT //To avoid a multiple definition, nautilus-python-object.c also includes and does the import.

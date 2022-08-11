6e48aa4ed699e326bf420e22428839f7b6f096c3
Jan Tojnar <jtojnar@gmail.com>
2022-08-11 02:08:52 +0200

    docs: Move examples into external files
    That way, we can easily test them and apply other development tools.

---
+++ examples/TestExtension.py
@@ -0,0 +1,28 @@
+from gi.repository import Nautilus, GObject
+
+class ColumnExtension(GObject.GObject, Nautilus.MenuProvider):
+    def __init__(self):
+        pass
+
+    def menu_activate_cb(self, menu, file):
+        print "menu_activate_cb",file
+
+    def get_file_items(self, window, files):
+        if len(files) != 1:
+            return
+
+        file = files[0]
+
+        item = Nautilus.MenuItem(
+            name="SimpleMenuExtension::Show_File_Name",
+            label="Showing %s" % file.get_name(),
+            tip="Showing %s" % file.get_name()
+        )
+        item.connect('activate', self.menu_activate_cb, file)
+
+        return [item]
+
+    # Even though we're not using background items, Nautilus will generate
+    # a warning if the method isn't present
+    def get_background_items(self, window, file):
+        return None
